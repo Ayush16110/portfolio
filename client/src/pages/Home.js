3a67056f5acdf285aedbe5632dd5cf6c0bc58344
Ayush16110 <newayushgupta2017@gmail.com>
2024-11-10 19:49:35 +0530

    Changes in Hero section css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Herosectionhome from '../components/Herosectionhome';
 import './global.css';
-import Divider from '../components/Divider';
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar/>
       <Herosectionhome/>
-      <Divider/>
     </div>
   );
 };
